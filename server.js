5102269d8b57ff8244821a427e6688b085db17bc
Riya priyadarsnai sahu <justriya2004@gmail.com>
2026-03-07 18:06:35 +0530

    change the port 3000 to 5000

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
   res.send("Auth API Running...");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = 5000;  
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
